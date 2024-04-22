76948ad6fb8ced2beadcd5e2f12e6d3deb2312bc
renec <92736972+rifahkhan@users.noreply.github.com>
2024-04-22 22:27:31 +0600

    report index error

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -9,11 +9,7 @@
 			</ol>
 		</div><br><!--/.row-->
 <!-- Modal -->
-@if ($message = Session::get('success'))
-<div class="alert alert-success alert-block">
-	<button type="button" class="close" data-dismiss="alert">×</button>	
-        <strong>{{ $message }}</strong>
-</div>
+
 @endif
 @if (count($errors) > 0)
         <div class="alert alert-danger">
